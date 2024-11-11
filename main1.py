2a00e2d9cc93669a4b590b2f15fc337240302a1a
wrestrus24 <varfolomee999777@gmail.com>
2024-11-11 23:14:48 +0700

    Добавлена функция

--- main1.py
+++ main1.py
@@ -1,3 +1,4 @@
+from logging import exception
 from tkinter import *
 from tkinter import messagebox as mb
 from tkinter import simpledialog as sd
@@ -5,6 +6,20 @@ import datetime
 import time
 import pygame
 
+
+def sett():
+    rem=sd.askstring("Время напоминания", "Введите время напоминания в формате ЧЧ:ММ")
+    if rem:
+        try:
+            hour=int(rem.split(":")[0])
+            minute=int(rem.split(":")[1])
+            now=datetime.datetime.now()
+            dt=now.replace(hour=hour, minute=minute, second=0)
+            label.config(text=f"Напоминание установлено на {hour}:{minute}")
+        except ValueError:
+            mb.showerror("ERROR","неверный формат времени")
+        except Exception as er:
+            pass
 window=Tk()
 window.title("Напоминание со звуком")
 
@@ -15,3 +30,4 @@ button=Button(text="Установить", command=sett)
 button.pack(pady=20)
 
 window.mainloop()
+
